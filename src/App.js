45043e2b445b3806e15c9c6a28ec6247706dcac4
marikomands <marikomands@gmail.com>
2023-08-10 15:49:25 +0100

    created form with react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Name</label>
+        <input
+          id="name"
+          type="text"
+          {...register("name", {
+            required: "This Field is Required",
+            minLength: { value: 5, message: "at least 5 letters are required" },
+          })}
+        />
+        <p>{errors.name?.message}</p>
+        <label>e-mail Address</label>
+        <input
+          id="email"
+          type="email"
+          {...register("email", { required: "email address is required" })}
+        />
+        <p>{errors.email?.message}</p>
+
+        <label>Password</label>
+        <input
+          id="password"
+          type="password"
+          {...register("password", {
+            required: "Password is required",
+            minLength: { value: 6, message: "at least 6 letters are required" },
+          })}
+        />
+        <p>{errors.password?.message}</p>
+
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
